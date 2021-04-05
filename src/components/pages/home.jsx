fb60222b358df6c80d91a2f8b90e4c8bc5cb594f
Nathalia Rus <nathaliarus@gmail.com>
2021-04-05 16:04:21 +0100

    add assets and comments

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -3,14 +3,18 @@ import styled from 'styled-components';
 import { connect } from "react-redux";
 import {archiveTask} from "../../redux/actions";
 
+// Dont worry about mobile UI. Pretend this is only for Desktop.
+// Hence why the media query below has been added.
+// Feel free to add styling to the Desktop page wrapper on this styled component directly 
+// rather than creating another one underneath.
 
-const PageContainer = styled.div`
 
+const DesktopPageContainer = styled.div`
   @media (max-width: 768px) {
     display: none;
   }
-
 `
+
 const actionCreators = {
   archiveTask,
 }
@@ -23,17 +27,17 @@ const mapStateToProps = state => {
 class Home extends React.Component {
   render() {
     return (
-    <PageContainer>
+    <DesktopPageContainer>
       
       <div>This is the main page</div>
 
       <button onClick={() => {
       this.props.archiveTask(3)
     }}>
-      Example button: on clicking this, you archive the task of ID 3.
+      Redux action example: by clicking this, you archive the task of ID 3. Check what happens in the Redux Dev Tools.
       </button>
 
-    </PageContainer>)
+    </DesktopPageContainer>)
   }
 };
 
