0c6f51df44e6d935a3290e2c4d96019cc7e4691f
Nathalia Rus <nathaliarus@gmail.com>
2021-05-09 16:29:23 +0100

    starter assets

--- src/redux/data/gods.ts
+++ src/redux/data/gods.ts
@@ -1,4 +1,9 @@
 import { IGodObj, IQuoteObj } from 'stateInterfaces'
+import artemis from '../../assets/gods/artemis.png'
+import athena from '../../assets/gods/athena.png'
+import athena2 from '../../assets/gods/athena2.png'
+import hera from '../../assets/gods/hera.png'
+import dyonisus from '../../assets/gods/dyonisus.png'
 
 export const gods_quotes: IQuoteObj = {
     'a-q-1': {
@@ -9,7 +14,14 @@ export const gods_quotes: IQuoteObj = {
         source: 'Illiad book VI',
     },
     'ath-q-1': {
-        god_id: 'athenas',
+        god_id: 'athena',
+        author: 'homer',
+        text:
+            'The rose Dawn might have found them weeping still had not grey-eyed Athena slowed the night when night was most profound, and held the Dawn under the Ocean of the East.',
+        source: 'Illiad book VI',
+    },
+    'dem-q-1': {
+        god_id: 'demeter',
         author: 'homer',
         text:
             'The rose Dawn might have found them weeping still had not grey-eyed Athena slowed the night when night was most profound, and held the Dawn under the Ocean of the East.',
@@ -80,58 +92,66 @@ export const gods_list: string[] = [
 ]
 
 export const gods: IGodObj = {
-    artemis: {
-        id: 'artemis',
-        name: 'artemis',
-        main_quote_id: 'a-q-1',
-        image: 'artemis_url',
-    },
-    athena: {
-        id: 'athena',
-        name: 'athena',
-        main_quote_id: 'ath-q-1',
-        image: 'artemis_url',
-    },
-    dyonisus: {
-        id: 'dyonisus',
-        name: 'dyonisus',
-        main_quote_id: 'dio-q-1',
-        image: 'artemis_url',
-    },
-    zeus: {
-        id: 'zeus',
-        name: 'zeus',
-        main_quote_id: 'z-q-1',
-        image: 'artemis_url',
+    demeter: {
+        id: 'demeter',
+        name: 'demeter',
+        main_quote_id: 'dem-q-1',
+        image: athena2,
     },
     hera: {
         id: 'hera',
         name: 'hera',
         main_quote_id: 'h-q-1',
-        image: 'artemis_url',
+        image: hera,
     },
+    artemis: {
+        id: 'artemis',
+        name: 'artemis',
+        main_quote_id: 'a-q-1',
+        image: artemis,
+    },
+    athena: {
+        id: 'athena',
+        name: 'athena',
+        main_quote_id: 'ath-q-1',
+        image: athena,
+    },
+    dyonisus: {
+        id: 'dyonisus',
+        name: 'dyonisus',
+        main_quote_id: 'dio-q-1',
+        image: dyonisus,
+    },
+
+    zeus: {
+        id: 'zeus',
+        name: 'zeus',
+        main_quote_id: 'z-q-1',
+        image: artemis,
+    },
+
     apollo: {
         id: 'apollo',
         name: 'apollo',
         main_quote_id: 'apo-q-1',
-        image: 'artemis_url',
+        image: artemis,
     },
     hephaistos: {
         id: 'hephaistos',
         name: 'hephaistos',
         main_quote_id: 'heph-q-1',
-        image: 'artemis_url',
+        image: artemis,
     },
     poseidon: {
         id: 'poseidon',
         name: 'poseidon',
         main_quote_id: 'pos-q-1',
-        image: 'artemis_url',
+        image: artemis,
     },
     ares: {
         id: 'ares',
         name: 'ares',
         main_quote_id: 'ares-q-1',
-        image: 'artemis_url',
+        image: artemis,
     },
 }
