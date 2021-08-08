e70e49a161ab18424bd570a0e450213aaf1162e6
Nathalia Rus <nathaliarus@gmail.com>
2021-08-08 20:08:30 +0100

    making some atoms - like icons

---
+++ src/components/atoms/icons/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import museis_logo from '../../../assets/logos/museis_logo.svg'
+
+export enum IconsEnum {
+    'logo',
+    'something-else',
+}
+
+export enum IconDirectionEnum {
+    'up',
+    'down',
+}
+
+export type IconProps = {
+    icon: IconsEnum
+    orientation?: IconDirectionEnum
+    active?: boolean
+    color?: string
+    height?: string
+    width?: string
+}
+
+class Icon extends React.Component<IconProps, {}> {
+    renderIcon(
+        icon: IconsEnum,
+        orientation?: IconDirectionEnum,
+        active?: boolean
+    ) {
+        switch (icon) {
+            case IconsEnum.logo:
+                return (
+                    <img
+                        style={{
+                            width: this.props.width ? this.props.width : '19px',
+                            cursor: 'pointer',
+                        }}
+                        src={museis_logo}
+                        alt="museis_logo"
+                    />
+                )
+
+            default:
+                console.log('please chose an icon')
+        }
+    }
+    render() {
+        return this.renderIcon(
+            this.props.icon,
+            this.props.orientation && this.props.orientation
+        )
+    }
+}
+
+export default Icon
